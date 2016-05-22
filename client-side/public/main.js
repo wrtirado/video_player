8f63521e12a1049b795390bb3496926aa2e23eeb
wrtirado <wrtirado@gmail.com>
2016-05-21 18:13:40 -0600

    Implemented a mute button and a volume slider. Now have play/pause, timeline slider, volume controll, and a restart button

--- client-side/public/main.js
+++ client-side/public/main.js
@@ -1,16 +1,28 @@
 var video,
     playPause,
-    seekslider
+    seekSlider,
+    curtimetext,
+    durtimetext,
+    muteBtn,
+    volumeSlider
 
 function runPlayer(){
   // Set object references
   video = document.getElementById("Video1")
   playPause = document.getElementById("play")
-  seekslider = document.getElementById("seekslider")
+  seekSlider = document.getElementById("seekSlider")
+  curtimetext = document.getElementById("curtimetext")
+  durtimetext = document.getElementById("durtimetext")
+  muteBtn = document.getElementById("muteBtn")
+  volumeSlider = document.getElementById("volumeSlider")
   // Add event listeners
   playPause.addEventListener("click", playPauseVideo, false)
-  seekslider.addEventListener("change", vidSeek, false)
+  seekSlider.addEventListener("change", vidSeek, false)
   video.addEventListener("timeupdate", seekTimeUpdate, false)
+  muteBtn.addEventListener("click", vidMute, false)
+  volumeSlider.addEventListener("change", setVolume, false)
+
+
 }
 window.onload = runPlayer
 
@@ -29,17 +41,47 @@ function playPauseVideo() {
         video.currentTime = 0;
     }
 
-    function skip(value) {
-        var video = document.getElementById("Video1");
-        video.currentTime += value;
-    }
-
     function vidSeek (){
-      var seekTo = video.duration * (seekslider.value / 100)
+      var seekTo = video.duration * (seekSlider.value / 100)
       video.currentTime = seekTo
     }
 
     function seekTimeUpdate (){
       var newTime = video.currentTime * (100 / video.duration)
-      seekslider.value = newTime
+      seekSlider.value = newTime
+      var currentMins  = Math.floor(video.currentTime / 60)
+      var currentSecs  = Math.floor(video.currentTime - currentMins * 60)
+      var durationMins = Math.floor(video.duration / 60)
+      var durationSecs = Math.floor(video.duration - durationMins * 60)
+
+      if(currentSecs < 10){
+        currentSecs = "0"+currentSecs
+      }
+      if(durationSecs < 10){
+        durationSecs = "0"+durationSecs
+      }
+      if(currentMins < 10){
+        currentMins = "0"+currentMins
+      }
+      if(durationMins < 10){
+        durationMins = "0"+durationMins
+      }
+      curtimetext.textContent = currentMins + ":" + currentSecs
+      durtimetext.textContent = durationMins + ":" + durationSecs
+    }
+
+    function vidMute (){
+      if (video.muted) {
+         video.muted = false
+         muteBtn.textContent = "Mute"
+         volumeSlider.value = 100
+      } else {
+         video.muted = true
+         muteBtn.textContent = "Un-Mute"
+         volumeSlider.value = 0
+      }
+    }
+
+    function setVolume (){
+      video.volume = volumeSlider.value / 100
     }
