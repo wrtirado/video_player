5eaa34d8d52ea652b131e4bb7956edc0dcf1a4e1
wrtirado <wrtirado@gmail.com>
2016-09-12 17:12:03 -0600

    Removed some notes

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 var express = require('express'),
         app	= express(),
- bodyParser = require('body-parser'),
-       cors = require('cors'),
      logger = require('morgan'),
        path = require('path'),
        port = process.env.PORT || 8080
 
-app.use( bodyParser.json() )
-app.use( bodyParser.urlencoded( {extended : true} ) )
-app.use( cors() )
+
 app.use( logger('dev') )
 
 app.use(express.static(path.join(__dirname, './client-side/public')))
